d4b7e137ada6ea8c0e86110f3d8ec095b470f198
Sjviray814 <100005518+Sjviray814@users.noreply.github.com>
2024-03-12 01:17:52 -0400

    Create SecondHalfFeed.java

---
+++ src/main/java/frc/robot/commands/autocommands/SecondHalfFeed.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autocommands;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.autocommands.FeedOff;
+import frc.robot.commands.autocommands.FeedOn;
+import frc.robot.commands.autocommands.ShooterOff;
+import frc.robot.commands.autocommands.ShooterOn;
+import frc.robot.commands.autocommands.TimedDriveOut;
+import frc.robot.commands.autocommands.FirstHalfFeed;
+import frc.robot.commands.autocommands.SecondHalfFeed;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Swerve;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class SecondHalfFeed extends ParallelDeadlineGroup {
+
+  /** Creates a new StupidDriveOut. */
+  public SecondHalfFeed(Intake intake, Shooter shooter) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+        new IntakeToAngle(intake, 0),
+        new OscillateFeeder(shooter)
+    );
+  }
+}
