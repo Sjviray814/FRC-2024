1cb2d31ec6ddc791bc403104c198fe2e164d2558
Sjviray814 <100005518+Sjviray814@users.noreply.github.com>
2024-03-12 01:23:09 -0400

    Create OscillateFeeder.java

---
+++ src/main/java/frc/robot/commands/autocommands/OscillateFeeder.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autocommands;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class OscillateFeeder extends Command {
+  /** Creates a new TimedDriveOut. */
+  private Shooter shooter;
+  
+  public OscillateFeeder(Shooter shooter) {
+    this.shooter = shooter;
+
+    addRequirements(shooter);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(shooter.getShooterBeamBreak()){
+        shooter.feedSlow();
+      }
+      else{
+        shooter.feedBack();
+      }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+      shooter.shooterOff();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
