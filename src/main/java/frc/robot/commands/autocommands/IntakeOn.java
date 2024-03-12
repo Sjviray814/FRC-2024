3e90e13168707057276563bd960c1f5368f53465
Sjviray814 <100005518+Sjviray814@users.noreply.github.com>
2024-03-12 01:00:12 -0400

    Create IntakeOn.java

---
+++ src/main/java/frc/robot/commands/autocommands/IntakeOn.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autocommands;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+public class IntakeOn extends Command {
+  /** Creates a new TimedDriveOut. */
+  private Intake intake;
+  
+  public IntakeOn(Intake intake) {
+    this.intake = intake;
+
+    addRequirements(intake);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intake.intakeOn();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+      intake.intakeOn();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
